7912343b808561f5e118ded2496ab102a7876107
nitjanan <nitjanan.rs@gmail.com>
2022-01-05 15:41:38 +0700

    รีเซ็ตรหัสผ่าน

--- djangostock/urls.py
+++ djangostock/urls.py
@@ -34,20 +34,21 @@ urlpatterns = [
     path('account/create',views.signUpView,name="signUp"),
     path('account/login',views.signInView,name="signIn"),
     path('account/logout',views.signOutView,name="signOut"),
+
     path('reset_password/',
-     auth_views.PasswordResetView.as_view(),
+     auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"),
      name="reset_password"),
 
     path('reset_password_sent/', 
-        auth_views.PasswordResetDoneView.as_view(), 
+        auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"), 
         name="password_reset_done"),
 
     path('reset/<uidb64>/<token>/',
-     auth_views.PasswordResetConfirmView.as_view(), 
+     auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html"), 
      name="password_reset_confirm"),
 
     path('reset_password_complete/', 
-        auth_views.PasswordResetCompleteView.as_view(), 
+        auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"), 
         name="password_reset_complete"),
 
     path('search/',views.search,name="search"),
