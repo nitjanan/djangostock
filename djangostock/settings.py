15e163287400665e8a88c370200251224fd5514a
nitjanan <nitjan.rs@gmail.com>
2022-06-28 11:38:26 +0700

    แก้ไขรีเซ็ตรหัสผ่านเป็น 2 factor

--- djangostock/settings.py
+++ djangostock/settings.py
@@ -176,4 +176,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'southerngroup.stock@gmail.com'
-EMAIL_HOST_PASSWORD = 'southerngroup2022'
+EMAIL_HOST_PASSWORD = 'cqbrdkkwyiuzhthd'
