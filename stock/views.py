dc9e580ca72ab309de12d805072ebcd42fe61c1a
nitjanan <nitjan.rs@gmail.com>
2025-07-25 10:22:03 +0700

    CM + PO แก้คำนวนทศนิยมไม่ตรง

--- stock/views.py
+++ stock/views.py
@@ -3823,7 +3823,7 @@ def printComparePricePO(request, cp_id):
             return redirect('viewComparePricePO')
 
     bidder = ComparisonPriceDistributor.objects.filter(cp = cp_id).order_by('amount', 'cp')
-    itemName = ComparisonPriceItem.objects.filter(cp = cp_id).order_by('bidder__amount', 'cp')
+    itemName = ComparisonPriceItem.objects.filter(cp = cp_id).order_by('bidder__amount', 'id')
 
     baseSparesType = BaseSparesType.objects.all()
     context = {
@@ -3858,7 +3858,7 @@ def showComparePricePO(request, cp_id, mode):
     baseSparesType = BaseSparesType.objects.all()
 
     bidder = ComparisonPriceDistributor.objects.filter(cp = cp_id).order_by('amount', 'cp')
-    itemName = ComparisonPriceItem.objects.filter(cp = cp_id).order_by('bidder__amount', 'cp')
+    itemName = ComparisonPriceItem.objects.filter(cp = cp_id).order_by('bidder__amount', 'id')
 
     #หาว่าเป็นใบเปรียบเทียบ(ยอดเกิน 200,000) แบบอนุมัติ 2 คนหรือไม่
     isSpecialCP = is_special_approver_cp(cp_id)
@@ -4106,7 +4106,7 @@ def printCPApprove(request, cp_id, isFromHome):
     baseSparesType = BaseSparesType.objects.all()
 
     bidder = ComparisonPriceDistributor.objects.filter(cp = cp_id).order_by('amount', 'cp')
-    itemName = ComparisonPriceItem.objects.filter(cp = cp_id).order_by('bidder__amount', 'cp')
+    itemName = ComparisonPriceItem.objects.filter(cp = cp_id).order_by('bidder__amount', 'id')
 
     isApprover = False
     isExaminer = False
