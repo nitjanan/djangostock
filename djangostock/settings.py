034246c9c244adf116b0e92a0439dd93938c6af3
Userpc <Userpc@USER>
2026-09-07 11:14:18 +0700

    แก้ไขให้ เว็ปแอป stock และ djagoweight สามารถเปิดพร้อมกัน

--- djangostock/settings.py
+++ djangostock/settings.py
@@ -249,4 +249,12 @@ SESSION_SAVE_EVERY_REQUEST = True
 SESSION_COOKIE_HTTPONLY = True
 SESSION_COOKIE_SECURE = False # เปิดเมื่อใช้ HTTPS
 SESSION_COOKIE_SAMESITE = 'Lax'
+
+# แยก cookie ของแต่ละโปรเจกต์ (browser ไม่แยก cookie ตาม port)
+SESSION_COOKIE_NAME = 'djangostock_sessionid'
+SESSION_COOKIE_PATH = '/'
+SESSION_COOKIE_DOMAIN = None
+CSRF_COOKIE_NAME = 'djangostock_csrftoken'
+CSRF_COOKIE_PATH = '/'
+CSRF_COOKIE_DOMAIN = None
 '''
